fc2b0ef675cd0d34d85aa890bc49002e0a0d8156
root <1499697067@qq.com>
2019-05-01 15:01:12 +0800

    first

---
+++ connect.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#this script is used to connect the ssl vpn of BJUT.
+#before launch this script,you should install the MotionPro(command line version).
+MotionPro --host vpn.bjut.edu.cn --user 16074217 --passwd xi@oqi888 #usernmae=your student id,password=your password of your my1.bjut.edu.cn
