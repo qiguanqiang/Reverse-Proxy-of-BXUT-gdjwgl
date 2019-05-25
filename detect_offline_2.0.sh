a557510fdef4e41df03e7fd7863f7253566e580c
root <1499697067@qq.com>
2019-05-25 12:16:57 +0800

    detect offline script v2.0 updated

---
+++ detect_offline_2.0.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+LOG_PATH='/var/log/Mei_bjut'
+
+while true
+do
+	echo `w3m localhost`|grep 正方
+	if [ $? == 0 ]
+	then
+		echo `date +%Y-%m-%d_%H:%M:%S`': accessible!' >> $LOG_PATH/Mei_running.log
+		sleep 60
+	else
+		MotionPro -s && sh /root/proxy/connect.sh && sh /root/proxy/route_add.sh
+		nginx -s reload
+		echo `date +%Y-%m-%d %H:%M:%S`': Failed! Excuted connection scripts!' >> $LOG_PATH/Mei_running.log
+	fi
+done
