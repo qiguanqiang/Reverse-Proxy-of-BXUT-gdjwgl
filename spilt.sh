5d27ac9c08eccf2b0b41b2c2595e5a914c4a8281
root <1499697067@qq.com>
2019-05-26 14:41:41 +0800

    update function split detect_offline's logs

--- spilt.sh
+++ spilt.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-
 logs_path="/var/log/nginx/"
 logs_path_new="/etc/nginx/log/"
+detect_offline_log_path="/var/log/Mei_bjut"
+detect_offline_log="Mei_running.log"
+#20190526
 access_log="access.log"
 error_log="error.log"
-#define paths of the nginx and related logs.
-
 mv "${logs_path}${access_log}" ${logs_path_new}access/access_$(date -d "yesterday" +"%Y%m%d").log
 mv "${logs_path}${error_log}" ${logs_path_new}error/error_$(date -d "yesterday" +"%Y%m%d").log
-#split logs using mv command by date.
-
+mv $detect_offline_log_path/$detect_offline_log $detect_offline_log_path/running_$(date -d "yesterday" +"%Y%m%d").log
 nginx -s reload
-#reload the nginx for writing new logs.
