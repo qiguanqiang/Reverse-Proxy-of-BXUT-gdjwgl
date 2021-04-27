bf817e4160a1d52ba699bbc5f76ddc9ef80692ee
QI GuanQiang <44294299+ZenBuilds@users.noreply.github.com>
2021-04-27 22:09:16 +0800

    Update connect.sh

--- connect.sh
+++ connect.sh
@@ -2,4 +2,4 @@
 
 #this script is used to automatically connect the ssl vpn of BJUT.
 
-MotionPro --host vpn.bjut.edu.cn --user $username --passwd  $password #username is an availavle student id,password is the password of the username for vpn.bjut.edu.cn
+MotionPro --host vxx.bxxx.edu.cn --user $username --passwd  $password #username is an availavle student id,password is the password of the username for vpn.bjut.edu.cn
